4f5925a6a4d732b05b891b715ecb7546c02a4f9a
Ren Jiaqi <jrenjq@gmail.com>
2022-03-17 17:22:24 +0800

    add home data class

---
+++ app/src/main/java/com/example/ict2105_quiz2_prep/entity/Home.kt
@@ -0,0 +1,13 @@
+package com.example.ict2105_quiz2_prep.entity
+
+data class Home(
+    val sell: Int,
+    val list: Int,
+    val living: Int,
+    val rooms: Int,
+    val beds: Int,
+    val baths: Int,
+    val age: Int,
+    val acres: Float,
+    val taxes: Int
+)
